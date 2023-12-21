3ae1b98105966ae9f6b6ac44186a37d838929088
Miki Stec <mikulas@stec.sk>
2023-12-21 03:28:44 +0100

    CRA + Tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
